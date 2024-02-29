a66cfbe6b612458da932fbc0bd823b313bf93a49
himelmia535 <himelmia535@gmail.com>
2024-02-29 20:27:17 +0600

    structure

---
+++ js/phone.js
@@ -0,0 +1,7 @@
+// api load check
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    console.log(data);
+}
+loadPhone();
