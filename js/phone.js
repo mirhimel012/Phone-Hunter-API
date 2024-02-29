4f19248dd5e939810cc59f9560b9514b81df0389
himelmia535 <himelmia535@gmail.com>
2024-03-01 00:04:28 +0600

    conditional show all button

--- js/phone.js
+++ js/phone.js
@@ -8,14 +8,26 @@ const loadPhone = async (searchText) =>{
 
 
 const displayPhones = phones =>{
-    // console.log(phones)
+    console.log(phones)
 
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container cards before adding new cards
     phoneContainer.textContent = '';
 
+    // display show all button if there are more than 12 phones
+    const showAllContainer = document.getElementById('show-all-container')
+    if(phones.length > 12){
+        showAllContainer.classList.remove('hidden')
+    }
+    else{
+        showAllContainer.classList.add('hidden');
+    }
+
+    // display only first 12 phones
+    phones = phones.slice(0,12);
+
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
         // 2 create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
